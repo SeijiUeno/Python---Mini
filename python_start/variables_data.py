bc3a722bd063115b53f4d06ccc19afa6c8623377
Seiji Ueno <sueno-te@c3r13p5.42sp.org.br>
2024-08-01 14:44:00 -0300

    learning of Python

--- python_start/variables_data.py
+++ python_start/variables_data.py
@@ -1,1 +1,25 @@
-print("Hello Python World! the lack of main and ';' is unsettling")
+# tests division
+dv = '\n' + '-' * 42 + '\n'
+
+# Start of the learning Journey
+message = "\nHello Python World! the lack of main and ';' is unsettling\n"
+message_2 = "Python Zero to One"
+print(message + message_2 + dv)
+
+# Flexible String test
+test = "Test of Dynamic strings\n"
+print(test)
+test = "A smaller string"
+print(test + dv)
+
+# Strings...
+test1 = 'this is a string\n'
+test2 = "this is also a string"
+print(test1);
+print(test2 + dv);
+
+test3 = 'I told my friend, "Python is my favorite language!"\n'
+test4 = "The language 'Python' is named after Monty Python, not the snake."
+print(test3)
+print(test4 + dv)
+
