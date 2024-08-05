370081ad8c835a05caba2f229b419503b62a848f
Seiji Ueno <sueno-te@c1r7p2.42sp.org.br>
2024-08-05 18:10:26 -0300

    simple operation of lists

--- python_start/list.py
+++ python_start/list.py
@@ -12,13 +12,41 @@ motorcycles[0] = 'ducati'
 print("\nAfter modification -> " + str(motorcycles))
 
 #Add new element to the end of the list
-
+# append
 motorcycles.append('honda')
 print("\nAdding honda in the end with append():\n" + str(motorcycles))
-
+# insert
 motorcycles.insert(2,'harley')
 print("\nInserting new motorcycle in the list, position 2, using insert()\n" + str(motorcycles))
-
+# del
 del motorcycles[2]
 print("\nAfter using the Del Statement in position 2, [del]\n" + str(motorcycles))
+# pop
+popped_motorcycle = motorcycles.pop()
+print("\nPrinting motorcycles after .pop()\n" + str(motorcycles))
+print("It returns the popped item: " + popped_motorcycle + "\n")
+# removing an item by value, remove
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print("\nA " + too_expensive.title() + " is too expensive.")
+print("\nAfter removing ducati by value: \n" + str(motorcycles) + "\n")
+
+#Sorting Lists
+
+#sort
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.sort()
+print("\nAfter using .sort\n" + str(cars))
+#sort(reverse)
+cars.sort(reverse=True)
+print("\nAfter using .sort(reverse=True)\n" + str(cars))
+print ("\nSorting it temporarily (sort)\n" + str(sorted(cars)))
+#reverse 
+cars.reverse()
+print("\nAfter using .reverse\n" + str(cars))
+
+#length
+length = len(cars)
+print ("\nThe list size is " + str(length))
 
